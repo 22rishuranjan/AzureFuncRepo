166a852dfed645d0f073474ae01b6171868e8fb2
Rishu Ranjan <45946604+22rishuranjan@users.noreply.github.com>
2021-10-26 15:19:35 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+var person = [ {"name":"rishu",
+              "age":21,
+               "dob":"10-02-1996"},
+              {"name":"ranjan",
+              "age":31,
+               "dob":"10-02-1990"},
+          ]
